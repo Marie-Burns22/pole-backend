4a0554f3b7c30ec7c1f00aa0959bbde9181696bf
Marie-Burns22 <41268371+Marie-Burns22@users.noreply.github.com>
2019-10-07 21:44:14 -0700

    Add permit_params to activeadmin model for newly created models.

--- app/admin/comments.rb
+++ app/admin/comments.rb
@@ -1,17 +1,17 @@
-ActiveAdmin.register Student do
-  permit_params :name, :email, :credits
+ActiveAdmin.register Comment do
+  permit_params :title, :comment, :created_at
 
   # See permitted parameters documentation:
   # https://github.com/activeadmin/activeadmin/blob/master/docs/2-resource-customization.md#setting-up-strong-parameters
   #
   # Uncomment all parameters which should be permitted for assignment
   #
-  # permit_params :name, :email, :credits
+  # permit_params :title, :content
   #
   # or
   #
   # permit_params do
-  #   permitted = [:name, :email, :credits]
+  #   permitted = [:title, :content]
   #   permitted << :other if params[:action] == 'create' && current_user.admin?
   #   permitted
   # end
