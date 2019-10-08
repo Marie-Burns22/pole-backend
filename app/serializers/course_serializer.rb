17c0df26cb7147c5a6081a87001982b316562306
Marie-Burns22 <41268371+Marie-Burns22@users.noreply.github.com>
2019-10-08 14:24:34 -0700

    Removed has_and_belongs_to_many relationship

--- app/serializers/course_serializer.rb
+++ app/serializers/course_serializer.rb
@@ -4,5 +4,5 @@ class CourseSerializer
 
   has_many :time_slots, serializer: TimeSlotSerializer
   has_many :bookings, through: :time_slots, serializer: BookingSerializer
-  has_and_belongs_to_many :students, serializer: StudentSerializer
+  # has_and_belongs_to_many :students, serializer: StudentSerializer
 end
