06ff72590b9b714ed5ef94fb54ced2058bb2219b
Marie-Burns22 <41268371+Marie-Burns22@users.noreply.github.com>
2019-10-10 12:48:40 -0700

    Remove relationshops with time_slots

--- app/serializers/course_serializer.rb
+++ app/serializers/course_serializer.rb
@@ -2,7 +2,7 @@ class CourseSerializer
   include FastJsonapi::ObjectSerializer
   attributes :name, :size, :location, :description
 
-  has_many :time_slots, serializer: TimeSlotSerializer
-  has_many :bookings, through: :time_slots, serializer: BookingSerializer
+  # has_many :time_slots, serializer: TimeSlotSerializer
+  # has_many :bookings, through: :time_slots, serializer: BookingSerializer
   # has_and_belongs_to_many :students, serializer: StudentSerializer
 end
