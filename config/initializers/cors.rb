9f9aabce45114a06970f0f19f0ca999693d4b503
Marie-Burns22 <41268371+Marie-Burns22@users.noreply.github.com>
2019-10-14 17:22:17 -0700

    Change origins

--- config/initializers/cors.rb
+++ config/initializers/cors.rb
@@ -16,8 +16,7 @@
 # end
 Rails.application.config.middleware.insert_before 0, Rack::Cors do
   allow do
-    origins 'http://localhost:3000',
-    origins 'https://vmpole.netlify.com'
+    origins 'http://localhost:3000', 'https://vmpole.netlify.com'
 
     resource '*',
       headers: :any,
