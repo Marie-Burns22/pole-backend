616b167571bd13d2592fbc2f90c52e1317f1bcc6
Marie-Burns22 <41268371+Marie-Burns22@users.noreply.github.com>
2019-10-18 10:14:12 -0700

    Fix time_slot admin resource typo

--- app/admin/time_slots.rb
+++ app/admin/time_slots.rb
@@ -5,7 +5,7 @@ ActiveAdmin.register TimeSlot do
   form do |f|
     f.input :date, as: :datepicker
     f.input :time, :as => :select, :collection => [1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11, 12]
-    f.input :min, :as => :select, :collectin => [00, 15, 30, 45]
+    f.input :min, :as => :select, :collection => [00, 15, 30, 45]
     f.input :am_pm, :as => :radio, :collection => ["am", "pm"]
     f.input :status, :as => :radio, :collection => ["available", "requested","booked"]
     f.actions
