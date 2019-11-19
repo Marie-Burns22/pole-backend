8c6c4e5f76f5a6bf961e433c47e382a6d65301a4
Marie-Burns22 <41268371+Marie-Burns22@users.noreply.github.com>
2019-11-18 17:41:34 -0800

    Remove extra model, controller and serializer files.

--- app/serializers/booking_serializer.rb
+++
@@ -1,7 +0,0 @@
-class BookingSerializer
-  include FastJsonapi::ObjectSerializer
-  
-  belongs_to :student
-  belongs_to :time_slot, serializer: TimeSlotSerializer
-  has_one :course, through: :time_slot, serializer: CourseSerializer
-end
