73aa3c25c09c80f5bfd9afdc628018328796bdeb
Marie-Burns22 <41268371+Marie-Burns22@users.noreply.github.com>
2019-11-18 17:48:58 -0800

    Remove user from routes.

--- config/routes.rb
+++ config/routes.rb
@@ -1,7 +1,7 @@
 Rails.application.routes.draw do
   root 'welcome#index'
   devise_for :admin_users, ActiveAdmin::Devise.config
-  devise_for :users
+
   ActiveAdmin.routes(self)
   
   namespace :api do
