0941ceb39a8912dc4a294e5ee5d4e506d814bb4e
Marie-Burns22 <41268371+Marie-Burns22@users.noreply.github.com>
2019-11-19 18:27:48 -0800

    Add another origin to cors.

--- config/initializers/cors.rb
+++ config/initializers/cors.rb
@@ -16,7 +16,7 @@
 # end
 Rails.application.config.middleware.insert_before 0, Rack::Cors do
   allow do
-    origins 'http://localhost:3000', 'https://vmpole.netlify.com', 'https://vegasinmiamipole.com'
+    origins 'http://localhost:3000', 'https://vmpole.netlify.com', 'https://vegasinmiamipole.com', 'http://vegasinmiamipole.com'
 
     resource '*',
       headers: :any,
